8fdf974fca7383d1c22f26a49c173a244dec736a
Vinicius Junior <viniciusjunior.dev@gmail.com>
2025-11-03 16:17:47 -0300

    style: add header styles layout to desktop and mobile

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,17 +1,88 @@
-import { Header } from "@/components/DashboardHeader";
+"use client";
+
+import { DashboardHeader } from "@/components/DashboardHeader";
+import Box from "@mui/material/Box";
+import { useTheme } from "@mui/material/styles";
+import Toolbar from "@mui/material/Toolbar";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useCallback, useRef, useState } from "react";
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const theme = useTheme();
+
+  const [isDesktopNavigationExpanded, setIsDesktopNavigationExpanded] =
+    useState(true);
+  const [isMobileNavigationExpanded, setIsMobileNavigationExpanded] =
+    useState(false);
+
+  const isOverMdViewport = useMediaQuery(theme.breakpoints.up("md"));
+
+  const isNavigationExpanded = isOverMdViewport
+    ? isDesktopNavigationExpanded
+    : isMobileNavigationExpanded;
+
+  const setIsNavigationExpanded = useCallback(
+    (newExpanded: boolean) => {
+      if (isOverMdViewport) {
+        setIsDesktopNavigationExpanded(newExpanded);
+      } else {
+        setIsMobileNavigationExpanded(newExpanded);
+      }
+    },
+    [
+      isOverMdViewport,
+      setIsDesktopNavigationExpanded,
+      setIsMobileNavigationExpanded,
+    ]
+  );
+
+  const handleToggleHeaderMenu = useCallback(
+    (isExpanded: boolean) => {
+      setIsNavigationExpanded(isExpanded);
+    },
+    [setIsNavigationExpanded]
+  );
+
+  const layoutRef = useRef<HTMLDivElement>(null);
+
   return (
     <div style={{ display: "flex", minHeight: "100vh" }}>
-      {/* <Sidebar /> */}
-      <main style={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
-        <Header title="Chronos" />
-        <div style={{ flexGrow: 1, overflowY: "auto" }}>{children}</div>
-      </main>
+      <Box
+        ref={layoutRef}
+        sx={{
+          position: "relative",
+          display: "flex",
+          overflow: "hidden",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <DashboardHeader
+          logo=""
+          title=""
+          menuOpen={isNavigationExpanded}
+          onToggleMenu={handleToggleHeaderMenu}
+        />
+        {/* <DashboardSidebar
+          expanded={isNavigationExpanded}
+          setExpanded={setIsNavigationExpanded}
+          container={layoutRef?.current ?? undefined}
+        /> */}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            flex: 1,
+            minWidth: 0,
+          }}
+        >
+          <Toolbar sx={{ displayPrint: "none" }} />
+        </Box>
+      </Box>
     </div>
   );
 }
--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,11 +1,15 @@
-"use client";
-
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 import MuiAppBar from "@mui/material/AppBar";
-import Stack from "@mui/material/Stack";
-import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
+import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import Stack from "@mui/material/Stack";
 import Link from "next/link";
+import { useCallback } from "react";
 
 const AppBar = styled(MuiAppBar)(({ theme }) => ({
   borderWidth: 0,
@@ -26,17 +30,57 @@ const LogoContainer = styled("div")({
   },
 });
 
-export interface HeaderProps {
+export interface DashboardHeaderProps {
   logo?: React.ReactNode;
   title?: string;
-  menuOpen?: boolean;
-  onToggleMenu?: (open: boolean) => void;
+  menuOpen: boolean;
+  onToggleMenu: (open: boolean) => void;
 }
 
-export function Header({ logo, title, menuOpen, onToggleMenu }: HeaderProps) {
+export function DashboardHeader({
+  logo,
+  title,
+  menuOpen,
+  onToggleMenu,
+}: DashboardHeaderProps) {
+  const theme = useTheme();
+
+  const handleMenuOpen = useCallback(() => {
+    onToggleMenu(!menuOpen);
+  }, [menuOpen, onToggleMenu]);
+
+  const getMenuIcon = useCallback(
+    (isExpanded: boolean) => {
+      const expandMenuActionText = "Expand";
+      const collapseMenuActionText = "Collapse";
+
+      return (
+        <Tooltip
+          title={`${
+            isExpanded ? collapseMenuActionText : expandMenuActionText
+          } menu`}
+          enterDelay={1000}
+        >
+          <div>
+            <IconButton
+              size="small"
+              aria-label={`${
+                isExpanded ? collapseMenuActionText : expandMenuActionText
+              } navigation menu`}
+              onClick={handleMenuOpen}
+            >
+              {isExpanded ? <MenuOpenIcon /> : <MenuIcon />}
+            </IconButton>
+          </div>
+        </Tooltip>
+      );
+    },
+    [handleMenuOpen]
+  );
+
   return (
-    <AppBar color="inherit" position="fixed" sx={{ displayPrint: "none" }}>
-      <Toolbar sx={{ backgroundColor: "inherit", boxShadow: 0 }}>
+    <AppBar color="inherit" position="absolute" sx={{ displayPrint: "none" }}>
+      <Toolbar sx={{ backgroundColor: "inherit", mx: { xs: -0.75, sm: -1 } }}>
         <Stack
           direction="row"
           justifyContent="space-between"
@@ -47,15 +91,15 @@ export function Header({ logo, title, menuOpen, onToggleMenu }: HeaderProps) {
           }}
         >
           <Stack direction="row" alignItems="center">
-            {/* <Box sx={{ mr: 1 }}>{getMenuIcon(menuOpen)}</Box> */}
+            <Box sx={{ mr: 1 }}>{getMenuIcon(menuOpen)}</Box>
             <Link href="/" style={{ textDecoration: "none" }}>
               <Stack direction="row" alignItems="center">
                 {logo ? <LogoContainer>{logo}</LogoContainer> : null}
                 {title ? (
                   <Typography
                     variant="h6"
-                    color="secondary"
                     sx={{
+                      color: (theme.vars ?? theme).palette.primary.main,
                       fontWeight: "700",
                       ml: 1,
                       whiteSpace: "nowrap",
